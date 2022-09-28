341826412be77d3423d190578b635e135c184367
Jennika Wilson <j.r.a.112100@gmail.com>
2022-09-27 20:02:52 -0400

    Connected to DB

--- backend/index.js
+++ backend/index.js
@@ -10,9 +10,9 @@ dotenv.config();
 //connecting to mongo
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
     console.log('connected to database')
-}).catch((err) => console.log(err))
+}).catch((err) => console.log(err));
 
 //creating app
 app.listen(5500, () => {
     console.log('backend is running')
-}) 
+});

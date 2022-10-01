bf7ff9da7b89fcc9453a3dc016fc4966d273cee2
Jennika Wilson <j.r.a.112100@gmail.com>
2022-10-01 13:14:38 -0400

    Got reviews route done and corrected email error

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    // email: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     age: { type: Number, default: 18 },
     address: { type: String },

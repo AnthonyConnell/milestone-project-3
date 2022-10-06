0370d005c4847d4ca649cc6f5ecfbec63f374f94
Jennika Wilson <j.r.a.112100@gmail.com>
2022-10-05 20:12:14 -0400

    Fixed login error

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -15,7 +15,8 @@ export default function Login({ setShowLogin, setCurrentUsername }) {
       password: passwordRef.current.value,
     };
     try {
-      const res = await axios.post("/users/login", user);
+      const res = await axios.post("http://localhost:5500/users/login"
+, user);
       setCurrentUsername(res.data.username);
     //   myStorage.setItem('user', res.data.username);
       setShowLogin(false)
